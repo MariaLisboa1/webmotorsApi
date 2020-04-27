8cc3c0578051c5bfdfd6b55c8dbf1234ccf583c3
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-04-27 00:34:55 -0300

    config mongo

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const connect = () =>
-  mongoose.connect(process.env.MONGO_URL, {
+  mongoose.connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
